a4d4d1f6127c2febd53185bc7a4af10ea9006d0b
Adnan <el.adnan@tutanota.de>
2022-02-13 14:54:12 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -65,8 +65,8 @@ class App extends Component {
     // const fundWithSigner = contract.connect(signer);
     
     // const tx = 
-    await wContract.addCampaign('0x259c3bA0d310D37d1897561dB87aDe43CEe31Ed9',
-    "sdjkjsdsdkj", true, 50, 1644924091); 
+    // await wContract.addCampaign('0x259c3bA0d310D37d1897561dB87aDe43CEe31Ed9',
+    // "sdjkjsdsdkj", true, 50, 1644924091); 
 
     // await tx.wait();
    
@@ -132,21 +132,7 @@ class App extends Component {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: 
-          {/* {this.state.storageValue} */}
-          </div>
-      </div>
+          <h1 className="text-3xl font-bold underline">      Hello world!    </h1>
     );
   }
 }

1d64c85835bc044cb369992ae3e0a470db590a3a
Adnan <el.adnan@tutanota.de>
2022-02-27 14:02:51 +0100

    Cannot find module next/babel warning

--- client/components/navbar/Navbar.jsx
+++ client/components/navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 import {
   AiFillGithub,
   AiFillTwitterCircle,

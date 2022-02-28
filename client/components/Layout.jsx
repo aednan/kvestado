7ead4c3e4fd4e035591f02f987d70f76af0e32ca
Adnan <el.adnan@tutanota.de>
2022-02-28 11:09:23 +0100

    update layout

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import CommandPalette from "./CommandPalette";
 import Footer from "./Footer";
 import Header from "./Header";
 import Main from "./Main";
+import Navbar from "./Navbar";
 
-export default function Layout({ Component, pageProps }) {
+export default function Layout({ children }) {
   return (
-    <div className="flex h-full min-h-screen flex-col bg-green-300 pt-20">
-      <Header></Header>
-      {/* <Main></Main> */}
-      <Component {...pageProps} />
-      <Footer></Footer>
+    <div className="h-full min-h-screen ">
+      <CommandPalette />
+      <Navbar />
+      <div className="flex h-full min-h-screen flex-col bg-green-300 pt-20">
+        <Header></Header>
+        {children}
+        <Footer></Footer>
+      </div>
     </div>
   );
 }
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,15 +1,11 @@
-import CommandPalette from "../components/CommandPalette";
 import Layout from "../components/Layout";
-import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="h-full min-h-screen ">
-      <CommandPalette />
-      <Navbar />
-      <Layout Component={Component} pageProps={pageProps}></Layout>
-    </div>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 

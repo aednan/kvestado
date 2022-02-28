607972e1ae535515cf914e62dfb8e217eca519a0
Adnan <el.adnan@tutanota.de>
2022-02-28 11:36:59 +0100

    update

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CommandPalette from "./CommandPalette";
 import Footer from "./Footer";
-import Header from "./Header";
+// import Header from "./Header";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
@@ -9,8 +9,8 @@ export default function Layout({ children }) {
     <div className="h-full min-h-screen ">
       <CommandPalette />
       <Navbar />
-      <div className="flex h-full min-h-screen flex-col bg-green-300 pt-20">
-        <Header></Header>
+      <div className="flex h-full min-h-screen flex-col bg-green-300 pt-16">
+        {/* <Header></Header> */}
         {children}
         <Footer></Footer>
       </div>

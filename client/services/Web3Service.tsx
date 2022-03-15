231c486446d55ab915e0ceca0cc15555eee2aec4
Adnan <el.adnan@tutanota.de>
2022-03-15 17:23:00 +0100

    update

--- client/services/Web3Service.tsx
+++ client/services/Web3Service.tsx
@@ -2,34 +2,39 @@ import { ethers } from "ethers";
 import { arrayify, hashMessage } from "ethers/lib/utils";
 declare var window: any;
 
-export async function userAuthentication() {
+// challenge message is requested from the backend, and be sent back to the backend after being signed,
+// in exchange for a cookie or a jwt token after a successful authentication.
+export async function userAuthentication(challenge: string) {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   try {
     const signer = provider.getSigner();
     const signerAddress = await signer.getAddress();
-    const flatSig = await signer.signMessage("hallo x");
+    const signedMessage = await signer.signMessage(challenge);
 
     // public key
-    const pkey = ethers.utils.recoverPublicKey(
-      arrayify(hashMessage("hallo x")),
-      flatSig
-    );
-    console.log(pkey);
+    // const pkey = ethers.utils.recoverPublicKey(
+    //   arrayify(hashMessage(challenge)),
+    //   signature
+    // );
+    // console.log(pkey);
     // const ppkey = ethers.utils.computePublicKey(pkey);
     // console.log(ppkey);
 
-    console.log(flatSig);
+    // console.log(signedMessage);
     // hashed message
-    console.log(hashMessage("hallo x"));
+    // console.log(hashMessage(challenge));
 
-    const signA = ethers.utils.splitSignature(flatSig);
-    console.log(signA);
+    // const signA = ethers.utils.splitSignature(signedMessage);
+    // console.log(signA);
 
-    console.log(signerAddress);
+    // console.log(signerAddress);
 
-    if (signerAddress === ethers.utils.verifyMessage("hallo x", flatSig)) {
+    if (
+      signerAddress === ethers.utils.verifyMessage(challenge, signedMessage)
+    ) {
       // user authenticated
       // TODO: authentication should be done also in the backend
+      // if user doesn't exist, account will be created with the wallet address
       console.log(true);
     }
   } catch (e: any) {

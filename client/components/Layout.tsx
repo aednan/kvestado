53b4858705b57011f7c1d3e04a5f8c669a52f2cc
Adnan <el.adnan@tutanota.de>
2022-03-18 01:01:28 +0100

    update

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,32 +1,65 @@
-import { Router } from "next/router";
-import React, { ReactNode, SetStateAction, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
 import GlobalContextWrapper from "../contexts/GlobalContextWrapper";
-// import UserContext from "../contexts/CommandPaletteContext";
 import CommandPalette from "./CommandPalette";
 import Footer from "./Footer";
+import { ImSpinner2 } from "react-icons/im";
 // import Header from "./Header";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const divRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+  const [loaded, setLoaded] = useState(true);
 
   useEffect(() => {
-    // on route navigation only
-    // window scrolling is disabled. This resets the scrolling within the div with ref: divRef
-    Router.events.on("routeChangeComplete", () => {
+    const handleRouteChangeStart = (url: any, { shallow }: any) => {
+      // console.log(
+      //   `App is changing to ${url} ${
+      //     shallow ? "with" : "without"
+      //   } shallow routing`
+      // );
+
+      // while page is loading
+      setLoaded(false);
+    };
+    const handleRouteChangeComplete = (url: any, { shallow }: any) => {
+      // on route navigation only
+      // window scrolling is disabled. This resets the scrolling within the div with ref: divRef
       divRef.current?.scroll({
         top: 0,
         left: 0,
         behavior: "smooth",
       });
-    });
-  });
 
-  // // for Command Palette Search bar
-  // const [isOpen, setIsOpen] = useState<boolean>(false);
+      // after page loaded
+      setLoaded(true);
+      // console.log(
+      //   `page is loaded: ${url} ${shallow ? "with" : "without"} shallow routing`
+      // );
+    };
+    router.events.on("routeChangeStart", handleRouteChangeStart);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
 
-  return (
-    // <UserContext.Provider value={{ setIsOpen, isOpen }}>
+    // on route navigation only
+    // window scrolling is disabled. This resets the scrolling within the div with ref: divRef
+    // Router.events.on("routeChangeComplete", () => {
+    //   divRef.current?.scroll({
+    //     top: 0,
+    //     left: 0,
+    //     behavior: "smooth",
+    //   });
+    // });
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+    };
+  }, []);
+
+  return loaded ? (
     <GlobalContextWrapper>
       <div className="fixed z-0 h-full min-h-screen w-full">
         <CommandPalette />
@@ -41,6 +74,9 @@ export default function Layout({ children }: { children: ReactNode }) {
         </div>
       </div>
     </GlobalContextWrapper>
-    // </UserContext.Provider>
+  ) : (
+    <div className="flex h-full min-h-screen w-full  bg-gray-100">
+      <ImSpinner2 className="m-auto animate-[spin_1.5s_linear_infinite] cursor-default text-9xl" />
+    </div>
   );
 }

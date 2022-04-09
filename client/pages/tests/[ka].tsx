88a0adf58b4c53d5ebebee76c0eaffe683a1a431
Adnan <el.adnan@tutanota.de>
2022-04-09 22:58:43 +0200

    update

--- client/pages/tests/[ka].tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const Kaa = (props: Props) => {
-  return <div className="bg-green-300 pt-72">hjjhhjkkhhjhjjgkhkgfdggytgu</div>;
-};
-
-export default Kaa;

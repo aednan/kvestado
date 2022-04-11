d843c900915069ac5be6a906fbd9987cb04c829a
Adnan <el.adnan@tutanota.de>
2022-04-11 14:29:06 +0200

    Heroku database config update - DB-URL-SUPPORT - [skip cd]

---
+++ backend/src/main/java/com/kvestado/backend/config/DataSourceConfig.java
@@ -0,0 +1,34 @@
+package com.kvestado.backend.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+@Profile("prod")
+public class DataSourceConfig {
+
+    @Bean
+    @Primary
+//    @ConfigurationProperties("spring.datasource")
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String url = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        return DataSourceBuilder
+                .create()
+                .username(username)
+                .password(password)
+                .url(url)
+//                .driverClassName("")
+                .build();
+    }
+}
